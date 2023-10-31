800ab48c1d9ec3df47c864198b053c65f9465ff6
Moriya Ramati <Moriyaramati1@gmail.com>
2023-10-31 09:11:41 +0200

    Initialize report model class

---
+++ Models/ReportModel.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Rebar.Models
+{
+    public class ReportModel
+    {
+
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public DateTime date { get; set; }
+        public int ordersAmount { get; set; }
+        public double totalDailyIncome { set; get; }
+
+    }    
+}
+
