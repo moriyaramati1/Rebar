53005ab904b892f7c4cbbde0908198af636b778d
Moriya Ramati <Moriyaramati1@gmail.com>
2023-10-31 09:17:32 +0200

    Initialize Accountancy class

---
+++ Models/Accountancy.cs
@@ -0,0 +1,20 @@
+﻿namespace Rebar.Models
+{
+    public class Accountancy
+    {
+        public List<ClientOrder> orders { set; get; }
+        public double totalDailyIncome { set; get; }
+
+        public Accountancy()
+        {
+            this.orders = new List<ClientOrder>();
+            this.totalDailyIncome = 0;
+        }
+
+        public void Reset()
+        {
+            this.orders.Clear();
+            this.totalDailyIncome = 0;
+        }
+    }
+}
