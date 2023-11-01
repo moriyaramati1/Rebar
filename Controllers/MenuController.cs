48f7b3c9c77ca11e221118d4f8164df2d944a0c3
Moriya Ramati <Moriyaramati1@gmail.com>
2023-11-01 17:50:58 +0200

    Edit MenuController.cs

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,18 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
 using Rebar.Models;
-using Rebar.DataAccess;
 using Rebar.Services;
-using System.Collections.Generic;
-using System.Net;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Rebar.Controllers
 {
     [ApiController]
 
-    [Route("api/shake")]
+    [Route("api/Rebar")]
     public class MenuController : ControllerBase
     {
         private readonly MenuService _menuService;
@@ -23,34 +18,71 @@ namespace Rebar.Controllers
             _menuService = new MenuService();
         }
 
-
-        [HttpGet]
-        public Task<List<Shake>?> Get()
-        {
-           
-            Task<List<Shake>?> result = _menuService.GetMenu();
-            Console.WriteLine(result);
-            return result;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> PostNewShake([FromBody] Shake shake)
+        [HttpPost("createBasicMenu")]
+        public IActionResult PostCreateBasicMenu()
         {
             try
             {
-                var result =_menuService.CreateNewShake(shake);
-               
+                _menuService.CreateMenu();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok();
+        }
+
+
+        [HttpGet("menu")]
+        public Task<List<Shake>?> Get()
+        {
+
+            Task<List<Shake>?> result = _menuService.GetMenu();
+
+            return result;
+
+
+        }
+
+        [HttpPost("newShake")]
+        public async Task<ActionResult> PostNewShake([FromBody] Shake shake)
+        {
+
+            try
+            {
+                var result = _menuService.CreateNewShake(shake);
+
             }
             catch (Exception ex)
             {
                 return BadRequest("An error occurred while creating the shake: " + ex.Message);
             }
+
             return Ok();
         }
 
-        [HttpPost]
-        public Task<ActionResult> PostNewOrder(ClientOrder order)
+        [HttpPost("newListShake")]
+        public async Task<ActionResult> PostNewShakes([FromBody] List<Shake> shakes)
         {
+
+            try
+            {
+                var result = _menuService.MultipleNewShake(shakes);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("An error occurred while creating the shakes: " + ex.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpPost("newOrder")]
+        public IActionResult PostNewOrder([FromBody] ClientOrder order)
+        {
+
             try
             {
                 var result = _menuService.TakeOrder(order);
@@ -58,52 +90,25 @@ namespace Rebar.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest("jjj " + ex.Message);
+                return BadRequest("Failure: " + ex.Message);
             }
-            return Ok();
+
+            return this.Ok("Your order has been successfully received");
         }
 
-        /*
-         * 
-         * 
-         * 
-         * 
-         * 
-      
-        
-
-         * 
-         * 
-        [HttpGet]
-        public async Task<List<Shake>> PostNewShake()
+        [HttpGet("dailyReport")]
+        public IActionResult GetDailyReport(int password)
         {
-            return await _menuService.GetMenu();
+            try
+            {
+                string message = _menuService.EndOfDay(password);
+                return this.Ok(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Failure: " + ex.Message);
+            }
+
         }
-         * [HttpGet]
-        public void GetCheckout()
-        {
-             _menuService.EndOfDay();
-            return 
-        }
-         Product item = repository.Get(id);
-    if (item == null)
-    {
-        var message = string.Format("Product with id = {0} not found", id);
-        HttpError err = new HttpError(message);
-        return Request.CreateResponse(HttpStatusCode.NotFound, err);
-    }
-    else
-    {
-        return Request.CreateResponse(HttpStatusCode.OK, item);
-    }
-         
-         
-         */
-
-
-        //CreateNewShake
-        //TakeOrder
-        //EndOfDay
-
     }
 }
