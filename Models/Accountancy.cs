ef2b9c9e5f1991ba22b552b82bdd52cd93cdfba4
Moriya Ramati <Moriyaramati1@gmail.com>
2023-11-01 17:52:36 +0200

    Modifing Accountancy class

--- Models/Accountancy.cs
+++ Models/Accountancy.cs
@@ -5,16 +5,25 @@
         public List<ServerOrder> orders { set; get; }
         public double totalDailyIncome { set; get; }
 
+        private const int _manager_password = 1234;
+
         public Accountancy()
         {
             this.orders = new List<ServerOrder>();
             this.totalDailyIncome = 0;
         }
-
         public void Reset()
         {
             this.orders.Clear();
             this.totalDailyIncome = 0;
         }
+        public bool CheckAuthentication(int password)
+        {
+            if (password != _manager_password)
+                throw new Exception("Access denied, incorrect password");
+
+            return true;
+        }
+
     }
 }
