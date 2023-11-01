551c8857e5d4b2c4bd8b38ef6b724d5ed5d7728b
Moriya Ramati <Moriyaramati1@gmail.com>
2023-11-01 17:59:28 +0200

    Changing and adding functionality to service class

--- Services/MenuService.cs
+++ Services/MenuService.cs
@@ -6,38 +6,58 @@ namespace Rebar.Services
 {
     public class MenuService
     {
-        public List<Shake> menuShakes;
+        public List<Shake>? menuShakes;
         public Accountancy account;
         public MongoDataBase db;
 
         public MenuService()
         {
-            this.menuShakes = new List<Shake>();
-            this.account = new Accountancy();
+            
             this.db = new MongoDataBase();
+            try
+            {
+                this.menuShakes = db.GetShakeCollection();
+            }
+            catch
+            {
+                throw;
+            }
+            
         }
 
         public void CreateMenu()
         {
             Shake shake1 = new Shake("Oreo", "milkshake made with vanilla ice cream, milk, and Oreo cookies!");
             Shake shake2 = new Shake("Chocolate", "milkshake");
-            Shake shake3 = new Shake("Strawberry", "milkshake");
+            Shake shake3 = new Shake("Strawberry", "milkshake ");
             Shake shake4 = new Shake("Vanilla", "milkshake");
             Shake shake5 = new Shake("Mango", "milkshake");
-            db.CreateShake(shake1);
+  
+            try
+            {
 
-            this.menuShakes.Add(shake1);
-            this.menuShakes.Add(shake2);
-            this.menuShakes.Add(shake3);
-            this.menuShakes.Add(shake4);
-            this.menuShakes.Add(shake5);
-            System.Console.WriteLine("PRINT THE MENU");
+                CreateNewShake(shake1);
+                CreateNewShake(shake2);
+                CreateNewShake(shake3);
+                CreateNewShake(shake4);
+                CreateNewShake(shake5);
+                
+                this.menuShakes.Add(shake1);
+                this.menuShakes.Add(shake2);
+                this.menuShakes.Add(shake3);
+                this.menuShakes.Add(shake4);
+                this.menuShakes.Add(shake5);
+            }
+            catch
+            {
+                throw;
+            }
 
         }
+
         public Task<List<Shake>?> GetMenu()
         {
-
-            return db.GetShakeCollection();
+            return db.GetShakeCollectionAsync();
         }
 
         public bool CheckValidName(string? name)
@@ -65,6 +85,7 @@ namespace Rebar.Services
             return true;
 
         }
+
         public void CheckValidAmount(List<OrderItem>? orderedShakes)
         {
             if (orderedShakes.Count > 10)
@@ -72,15 +93,16 @@ namespace Rebar.Services
                 throw new Exception("There are more than 10 shakes in this order.");
             }
         }
+
         public void CheckIfEmptyOrder(List<OrderItem>? orderedShakes)
         {
-            if (orderedShakes == null || orderedShakes.Count == 0 )
+            if (orderedShakes == null || !orderedShakes.Any())
             {
                 throw new Exception("Order content is empty");
             }
         }
 
-        public void CheckValidShakes(List<OrderItem> orderedShakes)
+        public void CheckIfExistInMenu(List<OrderItem> orderedShakes)
         {
 
             foreach (var shake in orderedShakes)
@@ -88,12 +110,12 @@ namespace Rebar.Services
                 var found = menuShakes.Find(item => item.uId == shake.id);
                 if (found == null)
                 {
-                    Console.WriteLine(found);
                     throw new Exception("Shake does not exist in menu");
 
                 }
             }
         }
+
         public void CheckValidOrder(ClientOrder order)
         {
             if (CheckValidName(order.customerName) == false)
@@ -104,7 +126,7 @@ namespace Rebar.Services
             {
                 CheckIfEmptyOrder(order.orderItems);
                 CheckValidAmount(order.orderItems);
-                CheckValidShakes(order.orderItems);
+                CheckIfExistInMenu(order.orderItems);
             }
             catch
             {
@@ -112,49 +134,66 @@ namespace Rebar.Services
             }
 
         }
-    
-        public async Task CreateNewShake(Shake newShake)
+
+        public void CheckUniqueName(Shake shake)
+        {
+            var found = menuShakes.Find(item => item.name == shake.name);
+            Console.WriteLine(found);
+            if (found != null)
+            {
+                throw new Exception("Another shake with similar name already exist");
+            }
+        }
+
+        public async Task AddShakeToDB(Shake newShake)
+        {
+            await this.db.CreateShake(newShake); 
+
+        }
+
+        public bool CreateNewShake(Shake newShake)
         {
             try
             {
                 var res = CheckValidParams(newShake);
-                await this.db.CreateShake(newShake);
+                CheckUniqueName(newShake);
+                AddShakeToDB(newShake);
                 this.menuShakes.Add(newShake);
             }
             catch
             {
                 throw;
             }
+            return true;
             
 
         }
-
-        public bool CheckAuthentication()
+       public bool MultipleNewShake(List<Shake> shakes)
         {
-            // TO DO
-            Console.WriteLine("enter Your ps");
+            try
+            {
+                foreach(Shake shake in shakes)
+                {
+                    CreateNewShake(shake);
+                }
+            }
+            catch
+            {
+                throw;
+            }
             return true;
         }
 
 
-        public void UpdateAccountancy(ServerOrder order)
-        {
-            account.orders.Add(order);
-            if(order.orderItems != null)
-            {
-                account.totalDailyIncome += order.orderItems.Sum(elemnet => elemnet.price);
-
-            }
-        }
         public void AddOrderToDB(ServerOrder order)
         {
-            
+
             DateTime finish = order.CalculateFinishTime();
             if (order.orderItems != null)
             {
                 List<Guid>? shakesIds = order.orderItems.Select(element => element.id).ToList();
-                
-                _ = db.CreateOrder(new OrderModel()
+
+                _ = db.CreateOrder(new OrderDBModel()
                 {
                     startTime = order.date,
                     finishTime = finish,
@@ -165,19 +204,17 @@ namespace Rebar.Services
                 });
             }
         }
+
         public bool TakeOrder(ClientOrder newOrder)
         {
             try
             {
                 CheckValidOrder(newOrder);
                 ServerOrder order = new ServerOrder(newOrder.customerName, newOrder.orderItems);
-
                 order.CalculatePayment();
                 Console.WriteLine(order.totalPrice);
                 AddOrderToDB(order);
                 CheckIfAdded(order);
-                UpdateAccountancy(order);
-
             }
             catch
             {
@@ -187,30 +224,39 @@ namespace Rebar.Services
             return true;
 
         }
+
         public async void CheckIfAdded(ServerOrder order)
         {
             var result = await db.GetOrderFromDB(order);
             if (result == null)
             {
-                throw new Exception("Failed to save an order");
+                throw new Exception("Failed to save the order");
             }
         }
         
-        public void EndOfDay()
+        public string EndOfDay(int password)
         {
-            CheckAuthentication();
-            int amount = account.orders.Count;
-            double ordersProfit = account.totalDailyIncome;
+            this.account = new Accountancy();
+            account.CheckAuthentication(password);
+            
+            var ordersList = db.GetTodayOrders();
+            
+            int amount = ordersList == null ? 0 : ordersList.Count();
+
+            account.totalDailyIncome = ordersList == null ? 0 : ordersList.Sum(o => o.totalPrice); ;
+
+            string message = $"Total number of orders today {amount} \n Total number of orders today {account.totalDailyIncome} \n";
+
 
             db.CreateReport(new ReportModel()
             {
                 date = DateTime.Today.Date,
                 ordersAmount = amount,
-                totalDailyIncome = ordersProfit
+                totalDailyIncome = account.totalDailyIncome
             });
 
             account.Reset();
-
+            return message;
         }
 
     }
